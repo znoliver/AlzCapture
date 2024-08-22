81e6bc9151b3064f818fc531bcd0ddf9fed1d7de
oliver <zlznoliver@gmail.com>
2024-08-22 17:30:24 +0800

    feat: 修改背景

--- AlzCapture/ViewModels/ProcessMonitorViewModel.cs
+++ AlzCapture/ViewModels/ProcessMonitorViewModel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Text;
 using AlzCapture.Businesses;
 using AlzCapture.Businesses.Interfaces;
